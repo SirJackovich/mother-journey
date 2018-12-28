c8d8bcd27cf47e6fb702f9fc8961e14bcc03c160
Jacob Reid <jacob.reid@familysearch.org>
2018-12-27 17:31:56 -0700

    Finished resources page

--- src/views/Resources.vue
+++ src/views/Resources.vue
@@ -1,25 +1,66 @@
 <template>
   <div id="resources">
     <h2>Resources</h2>
-    <div v-for="resource in resources" :key="resource.id">
-      <a :href="resource.link" target="_blank">
-        <div class="box">
-          <img :src="resource.image"><p>{{resource.title}}</p>
+    <div class="resource" v-for="resource in resources" :key="resource.id">
+      <a class="box" :href="resource.link" target="_blank">
+        <div class="image">
+          <img :src="resource.image">
         </div>
+        <p>{{resource.title}}</p>
       </a>
-      <p>{{resource.description}}</p>
+      <p class="description">{{resource.description}}</p>
     </div>
-
-
   </div>
 </template>
 
 <style lang="stylus">
   @import "../assets/styles/common.styl"
   #resources{
-    a {
+    h2 {
+      color: color-blue;
+    }
+    .resource {
+      margin-bottom: 50px;
+      display: flex;
+      align-items: center;
+      padding: 0 110px;
+      height: 200px;
       .box {
         border: 1px solid color-green;
+        border-radius: 25px;
+        margin-right: 25px;
+        padding: 10px;
+        width: 50%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        .image {
+          width: calc(50% - 25px)
+          margin-right: 25px;
+          height: 100%;
+          display: flex;
+          align-items: center;
+          img {
+            max-width: 100%;
+            max-height: 100%;
+          }
+        }
+        p {
+          text-align: center;
+          color: black;
+          font-weight: bold;
+          margin: 0;
+        }
+      }
+      .box:hover {
+        background-color: white;
+        text-decoration: none;
+      }
+      .description {
+        color: color-blue;
+        text-align: left;
+        margin: 0;
+        width: 50%;
       }
     }
   }
@@ -31,7 +72,7 @@
         resources: [
           {
             title: "Utah Valley Pediatrics",
-            image: require("../assets/img/uvpediatrics-logo.jpeg"),
+            image: require("../assets/img/uvpediatrics-logo.png"),
             link: "https://www.uvpediatrics.com",
             description: "We have been going to this clinic for over 30 years. Lots of useful information on this website."
           },
